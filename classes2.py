c36e7011b3098dc9bfdb535d1432f874b1e5e407
LackHaus <thomas.lacasse08@gmail.com>
2024-04-30 13:48:34 -0400

    Its getting somewhere

--- classes2.py
+++ classes2.py
@@ -21,9 +21,10 @@ from gdsfactory.config import print_version_pdks, print_version_plugins
 import random
 
 
-top = gf.Component()
+
 amf_mmi_x = 55
 amf_mmi_y = 20
+amf_mmi_port_dy = 1.8/2
 amf_pbrs_x = 413
 amf_pbrs_y = 98.55
 amf_edge_coupler_x = 370
@@ -37,7 +38,48 @@ amf_pbrs = PDK.get_component("AMF_Si_PBRS_Cband_v3p0")
 rings = gf.components.ring_crow(gaps=[0.2, 0.2, 0.2], radius=[10, 10])
 amf_pd = PDK.get_component("AMF_Ge_PD56G_Cband_v3p0")
 
-top.add_ref(amf_pd)
+# Definition of TOP cell
+top = gf.Component("TOP")
+
+# Initialization of Edge coupler
+EC_1 = top.add_ref(amf_edge_coupler)
+
+# Building the Fanout
+origin = [100, 0]
+fanout = []
+scaling_x = 2
+scaling_y = 2
+dy = amf_mmi_y*scaling_y
+dx = amf_mmi_x*scaling_x
+channels = 4
+for i in range(int(np.log2(channels))):
+    l = []
+    for j in range(2**i):
+        l.append(top.add_ref(amf_mmi).move((origin[0]+i*dx, origin[1] + j*dy - i*amf_mmi_port_dy)))
+    fanout.append(l)
+
+route = gf.routing.get_route(EC_1.ports["o1"], fanout[0][0].ports["o1"])
+top.add(route.references)
+
+dr = 2
+for i in range(len(fanout)-1):
+    rmin = 10
+    for j in range(len(fanout[i])):
+        route = gf.routing.get_route(fanout[i][j].ports["o3"], fanout[i+1][2*j].ports["o1"], radius = rmin)
+        top.add(route.references)
+        rmin = rmin + dr
+        route = gf.routing.get_route(fanout[i][j].ports["o2"], fanout[i+1][2*j+1].ports["o1"], radius = rmin)
+        top.add(route.references)
+        rmin = rmin + dr
+
+# PBRs array
+origin[0] = origin[0] + 250
+origin[1] = origin[1]
+for i in range(channels):
+
+    pbrs = top.add_ref(amf_pbrs).move((origin[0], i*origin[1]))
+    pbrs = top.add_ref(amf_pbrs).move((origin[0], i*amf_pbrs_y * scaling_y + origin[1]))
+
 
 
 top.show()
