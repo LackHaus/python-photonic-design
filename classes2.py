a47bdc2dfefdc84f6317565fd319bc1ca2a00762
LackHaus <thomas.lacasse08@gmail.com>
2024-04-30 16:42:01 -0400

    Starting PD array (Si)

--- classes2.py
+++ classes2.py
@@ -20,13 +20,15 @@ from gdsfactory.typings import Layer
 from gdsfactory.config import print_version_pdks, print_version_plugins
 import random
 
-
-
 amf_mmi_x = 55
 amf_mmi_y = 20
 amf_mmi_port_dy = 1.8/2
 amf_pbrs_x = 413
 amf_pbrs_y = 98.55
+amf_pbrs_port_dy = 1.8
+amf_pbrc_x = 413
+amf_pbrc_y = 98.55
+amf_pbrc_port_dy = 1.8
 amf_edge_coupler_x = 370
 amf_edge_coupler_y = 127
 amf_pd_x = 190.9
@@ -35,7 +37,7 @@ amf_pd_y = 170
 amf_mmi = PDK.get_component("AMF_Si_1X2MMI_Cband_v3p0")
 amf_edge_coupler = PDK.get_component("AMF_Si_EdgeCoupler_Cband_v3p0").rotate(180)
 amf_pbrs = PDK.get_component("AMF_Si_PBRS_Cband_v3p0")
-rings = gf.components.ring_crow(gaps=[0.2, 0.2, 0.2], radius=[10, 10])
+amf_pbrc = PDK.get_component("AMF_Si_PBRC_Cband_v3p0")
 amf_pd = PDK.get_component("AMF_Ge_PD56G_Cband_v3p0")
 
 # Definition of TOP cell
@@ -72,15 +74,81 @@ for i in range(len(fanout)-1):
         top.add(route.references)
         rmin = rmin + dr
 
-# PBRs array
+# PBRS array
 origin[0] = origin[0] + 250
 origin[1] = origin[1]
+pbrs = []
+scaling_y = 1.1
+bend = 10
 for i in range(channels):
+    pbrs.append(top.add_ref(amf_pbrs).move((origin[0], i*amf_pbrs_y * scaling_y + origin[1] - amf_pbrs_port_dy)))
+for c,v in enumerate(fanout[-1]):
+    bend = bend + 2*2*c
+    route = gf.routing.get_route(v.ports["o3"], pbrs[2*c].ports["o1"], radius=bend)
+    top.add(route.references)
+    bend = bend + 2*(2*c+1)
+    route = gf.routing.get_route(v.ports["o2"], pbrs[2*c+1].ports["o1"], radius=bend)
+    top.add(route.references)
 
-    pbrs = top.add_ref(amf_pbrs).move((origin[0], i*origin[1]))
-    pbrs = top.add_ref(amf_pbrs).move((origin[0], i*amf_pbrs_y * scaling_y + origin[1]))
+
+#Ring stage 1
+origin[0] = origin[0] + 600
+origin[1] = origin[1]
+rings_1 = []
+rings_2 = []
+radii1 = [[10, 10], [9, 9], [8, 8], [7, 7]]
+radii2 = [[9.5, 9.5], [8.5, 8.5], [7.5, 7.5], [6.5, 6.5]]
+gaps1 = [[0.2, 0.2, 0.2], [0.3, 0.3, 0.3], [0.4, 0.4, 0.4], [0.5, 0.5, 0.5]] 
+gaps2 = [[0.25, 0.25, 0.25], [0.35, 0.35, 0.35], [0.45, 0.45, 0.45], [0.5, 0.5, 0.5]] 
+dy = 150
+dx = 200
+ring_y_port = 1.8
+for i in range(channels):
+    rings1 = gf.components.ring_crow(gaps=gaps1[i], radius=radii1[i])
+    rings2 = gf.components.ring_crow(gaps=gaps2[i], radius=radii2[i]) 
+    r1 = []
+    r2 = []
+    for j in range(2):
+        r1.append(top.add_ref(rings1).move((origin[0], origin[1] + i*dy + (j)*dy/2 - ring_y_port)))
+        r2.append(top.add_ref(rings2).move((origin[0]+dx, origin[1] + i*dy + (j)*dy/2)))
+
+    rings_1.append(r1)
+    rings_2.append(r2)
+
+bend = 10
+for i in range(len(pbrs)):
+    route = gf.routing.get_route(pbrs[i].ports["o3"], rings_1[i][0].ports["o1"], radius=bend)
+    top.add(route.references)
+    bend = bend + 5
+    route = gf.routing.get_route(pbrs[i].ports["o2"], rings_1[i][1].ports["o1"], radius=bend)
+    top.add(route.references)
+    bend = bend + 5
+
+for i in range(len(rings_1)):
+    route = gf.routing.get_route(rings_1[i][0].ports["o4"], rings_2[i][0].ports["o3"], radius=10)
+    top.add(route.references)
+    route = gf.routing.get_route(rings_1[i][1].ports["o4"], rings_2[i][1].ports["o3"], radius=10)
+    top.add(route.references)
 
 
-
+# PBRC array
+origin[0] = origin[0] + 350
+origin[1] = origin[1]
+pbrc = []
+scaling_y = 1.1
+bend = 10
+for i in range(channels):
+    pbrc.append(top.add_ref(amf_pbrc).move((origin[0], i*amf_pbrc_y * scaling_y + origin[1] - amf_pbrc_port_dy)))
+bend = 10 + 2*2*channels
+for i in range(len(pbrc)):
+    route = gf.routing.get_route(rings_2[i][0].ports["o2"], pbrc[i].ports["o1"], radius = bend)
+    top.add(route.references)
+    bend = bend-2
+    route = gf.routing.get_route(rings_2[i][1].ports["o2"], pbrc[i].ports["o2"], radius = bend)
+    top.add(route.references)
+    bend = bend-2
 top.show()
 
+# PD array 
+
+pds = []
